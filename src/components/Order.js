7fc33946091834f3873e2b8ebf783bff3e1b050a
Saujanya Nagpal <saujanyanagpal104@gmail.com>
2018-06-24 19:49:06 +0530

    basic components

---
+++ src/components/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Order extends React.Component {
+  render() {
+    return(
+      <div className='order'>
+        <p>Order</p>
+      </div>
+    )
+  }
+}
+export default Order;

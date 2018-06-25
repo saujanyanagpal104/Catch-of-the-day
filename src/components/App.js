45ecc375b3b196a2362a369c92d135fce3f54c8a
Saujanya Nagpal <saujanyanagpal104@gmail.com>
2018-06-26 01:35:33 +0530

    adding sampleFishes to the state

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
@@ -13,6 +14,10 @@ class App extends React.Component {
     fishes[`fish${Date.now()}`] = fish;
     this.setState({ fishes: fishes})
   }
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes});
+  }
+
   render() {
     return(
       <div className="catch-of-the-day">
@@ -20,7 +25,7 @@ class App extends React.Component {
           <Header />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     )
   }

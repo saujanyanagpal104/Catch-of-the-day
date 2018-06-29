57d7e7af722f799c7e0381eb39a53a8a73bf0e71
Saujanya Nagpal <saujanyanagpal104@gmail.com>
2018-06-29 20:50:48 +0530

    adding and deleting items from inventory and order

--- src/components/App.js
+++ src/components/App.js
@@ -13,10 +13,20 @@ class App extends React.Component {
   };
   componentDidMount(){
     const { params } = this.props.match;
+    const localStorageRef = localStorage.getItem(params.storeId)
+    if(localStorageRef){
+      this.setState({ order: JSON.parse(localStorageRef)});
+    }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     });
+  }
+  componentDidUpdate(){
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    )
   }
 
   componentWillUnmount(){
@@ -27,6 +37,16 @@ class App extends React.Component {
     fishes[`fish${Date.now()}`] = fish;
     this.setState({ fishes: fishes})
   }
+  updateFish = (key, updatedFish) => {
+    const fishes = {...this.state.fishes }
+    fishes[key] = updatedFish,
+    this.setState({ fishes: fishes })
+  }
+  deleteFish = key => {
+  const fishes = { ...this.state.fishes };
+  fishes[key] = null;
+  this.setState({ fishes });
+};
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes});
   }
@@ -36,6 +56,12 @@ class App extends React.Component {
     order[key] = order[key] + 1 || 1;
     this.setState({ order : order });
   }
+  removeFromOrder = key => {
+  const order = { ...this.state.order };
+  delete order[key];
+  this.setState({ order })};
+
+
 
   render() {
     return(
@@ -46,8 +72,8 @@ class App extends React.Component {
             {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order}/>
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+        <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder}/>
+        <Inventory addFish={this.addFish} updateFish={this.updateFish} loadSampleFishes={this.loadSampleFishes} fishes={this.state.fishes} deleteFish={this.deleteFish} />
       </div>
     )
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,16 +1,38 @@
-import React from 'react';
-
-import AddFishForm from './AddFishForm';
+import React from "react";
+import PropTypes from "prop-types";
+import firebase from "firebase";
+import AddFishForm from "./AddFishForm";
+import EditFishForm from "./EditFishForm";
+import base, { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
+  static propTypes = {
+    fishes: PropTypes.object,
+    updateFish: PropTypes.func,
+    deleteFish: PropTypes.func,
+    loadSampleFishes: PropTypes.func
+  };
+
   render() {
     return (
       <div className="inventory">
-        <p>Inventory</p>
-        <AddFishForm addFish={this.props.addFish}/>
-        <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
+        <h2>Inventory</h2>
+        {Object.keys(this.props.fishes).map(key => (
+          <EditFishForm
+            key={key}
+            index={key}
+            fish={this.props.fishes[key]}
+            updateFish={this.props.updateFish}
+            deleteFish={this.props.deleteFish}
+          />
+        ))}
+        <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSampleFishes}>
+          Load Sample Fishes
+        </button>
       </div>
-    )
+    );
   }
 }
+
 export default Inventory;
